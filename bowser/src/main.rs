70a7db0a859525675d54543f68a56a111006e1ee
shaqque <shaqqywyan@gmail.com>
2022-10-31 00:26:01 -0500

    Fix hang until timeout bug

--- bowser/src/main.rs
+++ bowser/src/main.rs
@@ -1,11 +1,12 @@
-use std::env;
-use std::io::{Write, Read};
-use std::net::{TcpStream, Shutdown};
 use std::collections::HashMap;
+use std::env;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::TcpStream;
+use std::str;
 
 // cargo run http://example.com:80/index.html
 
-fn request(url:&String) -> (HashMap<String, String>, String) {
+fn request(url: &String) -> (HashMap<String, String>, String) {
     // parse url into host and path
     assert!(url.starts_with("http://"));
     let trimmed = url.strip_prefix("http://").expect("Unable to trim url!");
@@ -28,42 +29,36 @@ fn request(url:&String) -> (HashMap<String, String>, String) {
 
     // send HTTP request as bytes via TCP stream
     let req_bytes: &[u8] = http_req.as_bytes();
-    stream.write_all(req_bytes).expect("Failed to writes bytes for request!");
+    stream
+        .write_all(req_bytes)
+        .expect("Failed to writes bytes for request!");
     println!("Sent Request");
 
     // read the response via TCP stream
-    let mut http_res = String::new();
-    stream.read_to_string(&mut http_res).expect("Failed to read response!");
-    println!("Got Response");
-    println!("{}", http_res);
-
-    // idk if we need following lines: socket seems to auto-close
-    // // close the TCP connection
-    // stream.shutdown(Shutdown::Both).expect("Failed to shutdown socket!");
-    // println!("Closed Socket");
-
-    // parse the response into headers and body
-    let mut lines = http_res.lines();
-
-    let status_line = lines.next().expect("No lines");
+    let mut reader = BufReader::new(&stream);
+    println!("Waiting for response");
+    let mut status_line = String::new();
+    reader
+        .read_line(&mut status_line)
+        .expect("error reading status line");
     let mut status_line_split = status_line.split_whitespace();
     status_line_split.next();
     assert_eq!(Some("200"), status_line_split.next());
 
     let mut headers = HashMap::new();
     loop {
-        let line = lines.next();
-        match line {
-            None => { break; },
-            Some(inner) => {
-                if inner == "" { break; }
-                let mut split_line = inner.splitn(2, ":");
+        let mut line = String::new();
+        reader.read_line(&mut line).expect("error reading header");
+        match line.as_str() {
+            "\r\n" => {
+                break;
+            }
+            data => {
+                let mut split_line = data.splitn(2, ":");
                 let header = split_line.next().unwrap();
                 let value = split_line.next().unwrap();
                 headers.insert(header.to_lowercase(), value.trim().to_lowercase());
-                continue;
             }
-            other => { continue; }
         }
     }
 
@@ -72,31 +67,30 @@ fn request(url:&String) -> (HashMap<String, String>, String) {
     assert!(!headers.contains_key("transfer-encoding"));
     assert!(!headers.contains_key("content-encoding"));
 
-    let mut body = String::new();
-    loop {
-        let line = lines.next();
-        
-        match line {
-            None => { break; },
-            Some(inner) => {
-                body.push_str(inner);
-                continue;
-            }
-            other => { continue; }
+    let length = match headers.get("content-length") {
+        None => {
+            println!("no body content!");
+            0
         }
-    }
+        Some(x) => x.parse::<usize>().expect("error parsing content length"),
+    };
+    let mut body = vec![0u8; length];
+    reader.read_exact(&mut body).expect("error reading body");
+    let body = str::from_utf8(&body)
+        .expect("error converting [u8] to string")
+        .to_string();
 
     println!("BODY: {}", body);
 
     (headers, body)
 }
 
-fn load(url:&String) {
-    let (headers, body) = request(url);
+fn load(url: &String) {
+    let (_headers, body) = request(url);
     show(body);
 }
 
-fn show(body:String) {
+fn show(body: String) {
     let mut in_angle = false;
     for c in body.chars() {
         if c == '<' {
