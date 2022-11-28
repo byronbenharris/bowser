e0ac397bb1b60815a1197f993ac7a32d2c7a9f6c
byron-harris <bbh3@rice.edu>
2022-11-28 00:47:09 -0600

    modulated layout code

--- bowser/src/layout/mod.rs
+++ bowser/src/layout/mod.rs
@@ -1,14 +1,39 @@
-use std::collections::HashMap;
-use std::rc::Rc;
+use druid::widget::Label;
+use druid::{FontDescriptor, FontFamily, FontWeight, FontStyle, Widget};
 use std::cell::RefCell;
+use std::str;
+use std::rc::Rc;
 
 use crate::html::{DOMNode, Data};
 
 #[derive(Debug)]
 pub struct LayoutNode {
-    pub node: DOMNode,
+    pub node: Rc<RefCell<DOMNode>>,
     pub inline: bool,
     pub children: RefCell<Vec<Rc<LayoutNode>>>,
+}
+
+impl LayoutNode {
+    fn new(node: &Rc<RefCell<DOMNode>>, inline: bool) -> LayoutNode {
+        return LayoutNode { node: Rc::clone(node), inline, children: RefCell::new(vec!()) };
+    }
+
+    fn add_child(&mut self, child: &Rc<LayoutNode>) {
+        self.children.borrow_mut().push(Rc::clone(&child));
+    }
+}
+
+#[derive(Debug)]
+pub struct Style {
+    size: f64, 
+    bold: bool, 
+    italic: bool,
+}
+
+impl Style {
+    pub fn new() -> Style {
+        return Style { size: 16.0, bold: false, italic: false };
+    }
 }
 
 const BLOCK_ELEMENTS: [&str; 37] = [
@@ -19,6 +44,81 @@ const BLOCK_ELEMENTS: [&str; 37] = [
     "figcaption", "main", "div", "table", "form", "fieldset",
     "legend", "details", "summary"
 ];
+
+fn get_font(style: &Style) -> FontDescriptor {
+    return FontDescriptor::new(FontFamily::SERIF)
+        .with_size(style.size)
+        .with_weight(match style.bold {
+            true => FontWeight::BOLD,
+            false => FontWeight::REGULAR,
+        })
+        .with_style(match style.italic {
+            true => FontStyle::Italic,
+            false => FontStyle::Regular,
+        });
+}
+
+fn open_tag(tag: &String, style: &Style) -> Style {
+    match tag.as_str() {
+        "b" => { return Style { bold: true, ..(*style) } },
+        "i" => { return Style { italic: true, ..(*style) } },
+        "bigger" => { return Style { size: style.size + 2.0, ..(*style) } },
+        "smaller" => { return Style { size: style.size - 2.0, ..(*style) } },
+        _ => { return Style{ ..(*style) }; }
+    }
+}
+
+fn label(text: &String, style: &Style) -> impl Widget<()> {
+    let font = get_font(style);
+    let mut label = Label::new(text.as_str()).with_font(font);
+    label.set_line_break_mode(druid::widget::LineBreaking::WordWrap);
+    return label;
+}
+
+pub fn recurse(node: &Rc<RefCell<DOMNode>>, style: &Style) -> Vec<impl Widget<()>> {
+    match &node.borrow().data {
+        Data::Text(text) => {
+            let mut body = Vec::new();
+            body.push(label(&text.text, style)); 
+            return body;
+        },
+        Data::Element(elem) => { 
+            let style = open_tag(&elem.tag, style);
+            let mut body = Vec::new();
+            for child in &*node.borrow().children.borrow() {
+                for label in recurse(&Rc::clone(child), &style) {
+                    body.push(label);
+                }
+            }
+            return body;
+        }
+    }    
+}
+
+pub fn layout(node: &Rc<RefCell<DOMNode>>, style: &Style) -> Rc<LayoutNode> {
+    return Rc::new(LayoutNode::new(node, false));
+
+    // for child in &*node.borrow().children.borrow() {
+    //     layout(child, style);
+    // }
+
+
+    // match &node.borrow().data {
+    //     Data::Text(text) => { 
+    //         println!("{}", text.text);
+    //     },
+    //     Data::Element(elem) => { 
+    //         println!("<{}>", elem.tag);
+    //         for child in &*node.borrow().children.borrow() {
+    //             print_dom(child, indent + 2);
+    //         }
+    //         print_indent(indent);
+    //         println!("</{}>", elem.tag);
+    //     }
+    // }
+}
+
+pub fn render(node: &Rc<LayoutNode>) { }
 
 // class DocumentLayout:
 //     def __init__(self, node):
--- bowser/src/main.rs
+++ bowser/src/main.rs
@@ -1,39 +1,26 @@
 use druid::piet::InterpolationMode;
 use druid::widget::{FillStrat, Flex, Image, Label, Scroll};
-use druid::{AppLauncher, FontDescriptor, FontFamily, FontWeight, FontStyle, ImageBuf, LocalizedString, Widget, WindowDesc};
-use std::cell::RefCell;
+use druid::{AppLauncher, ImageBuf, Widget, WindowDesc};
 use std::{env, str};
 use std::rc::Rc;
 
-use bowser::html::{DOMNode, Data, print_dom, parse};
+use bowser::html::{print_dom, parse};
+use bowser::layout::{Style, recurse, layout, render};
 use bowser::request::request;
-
-#[derive(Debug)]
-struct Style {
-    size: f64, 
-    bold: bool, 
-    italic: bool,
-}
 
 fn load(url: &String) -> impl Widget<()> {
     let (headers, body) = request(url);
     assert!(headers.contains_key("content-type"));
-    let content_type = headers
-        .get("content-type")
-        .unwrap()
-        .splitn(2, ';')
-        .next()
-        .unwrap();
+    let content_type = headers.get("content-type").unwrap().splitn(2, ';').next().unwrap();
     let mut col = Flex::column();
     match content_type {
         "text/html" => {
             let body_str = str::from_utf8(&body).expect("Failed to convert [u8] to string");
             let dom_root = parse(&body_str.to_string());
             print_dom(&Rc::clone(&dom_root), 0);
-            // let layout_root = parse_layout(&Rc::clone(&dom_root), &Style { size: 16.0, bold: false, italic: false });
-            // draw_layout(&Rc::clone(&layout_root));
-            let body_widgets = recurse(
-                &Rc::clone(&dom_root), &Style { size: 16.0, bold: false, italic: false });
+            // let layout_root = layout(&Rc::clone(&dom_root), &Style::new());
+            // render(&Rc::clone(&layout_root));
+            let body_widgets = recurse(&Rc::clone(&dom_root), &Style::new());
             for widget in body_widgets {
                 col.add_child(widget);
             }
@@ -51,68 +38,10 @@ fn load(url: &String) -> impl Widget<()> {
     Scroll::new(col).vertical()
 }
 
-fn get_font(style: &Style) -> FontDescriptor {
-    return FontDescriptor::new(FontFamily::SERIF)
-        .with_size(style.size)
-        .with_weight(match style.bold {
-            true => FontWeight::BOLD,
-            false => FontWeight::REGULAR,
-        })
-        .with_style(match style.italic {
-            true => FontStyle::Italic,
-            false => FontStyle::Regular,
-        });
-}
-
-fn open_tag(tag: &String, style: &Style) -> Style {
-    match tag.as_str() {
-        "b" => { return Style { bold: true, ..(*style) } },
-        "i" => { return Style { italic: true, ..(*style) } },
-        "bigger" => { return Style { size: style.size + 2.0, ..(*style) } },
-        "smaller" => { return Style { size: style.size - 2.0, ..(*style) } },
-        _ => { return Style{ ..(*style) }; }
-    }
-}
-
-fn label(text: &String, style: &Style) -> impl Widget<()> {
-    let font = get_font(style);
-    let mut label = Label::new(text.as_str()).with_font(font);
-    label.set_line_break_mode(druid::widget::LineBreaking::WordWrap);
-    return label;
-}
-
-// fn parse_layout(node: &Rc<RefCell<DOMNode>>, style: &Style) -> LayoutNode {
-
-// }
-
-fn recurse(node: &Rc<RefCell<DOMNode>>, style: &Style) -> Vec<impl Widget<()>> {
-    match &node.borrow().data {
-        Data::Text(text) => {
-            let mut body = Vec::new();
-            body.push(label(&text.text, style)); 
-            return body;
-        },
-        Data::Element(elem) => { 
-            let style = open_tag(&elem.tag, style);
-            let mut body = Vec::new();
-            for child in &*node.borrow().children.borrow() {
-                for label in recurse(&Rc::clone(child), &style) {
-                    body.push(label);
-                }
-            }
-            return body;
-        }
-    }    
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
     AppLauncher::with_window(
-        WindowDesc::new(load(url))
-            .title(LocalizedString::new("Bowser Title")
-            .with_placeholder("Bowser")),
-    )
-    .launch(())
-    .expect("failed to launch gui");
+        WindowDesc::new(load(url)).title(String::from("Bowser"))
+    ).launch(()).expect("failed to launch gui");
 }
